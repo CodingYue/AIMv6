1db7532e88d29d02d7e9e6d7f87c6264cfc07f05
CodingYue <this.isssssyy@gmail.com>
2015-10-28 11:17:26 +0800

    bootloader

--- boot/arch/armv7a-le/zynq7000/bootmain.c
+++ boot/arch/armv7a-le/zynq7000/bootmain.c
@@ -11,14 +11,20 @@
 #include <config.h>
 
 #define PRELOAD_VECTOR_BASE	0x1ff00000
+#define MBR_SIZE 446
 
 void (*uart_init)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x4);
 void (*uart_enable)(void) = (void *)(PRELOAD_VECTOR_BASE + 0x8);
 void (*uart_spin_puts)(const char *) = (void *)(PRELOAD_VECTOR_BASE + 0xC);
+void (*puthex)(u32) = (void *) (PRELOAD_VECTOR_BASE + 0x10);
 
 void mbr_bootmain(void)
 {
-	uart_spin_puts("Hello, AIMv6!\r\n");
+	volatile u32 *mbr = (void *) 0x10002;
+	u32 LBA = (u32) mbr[111+2];
+	u32 block_number = (u32) (mbr[111+3]);
+	puthex(LBA);
+	puthex(block_number);
 	while(1);
 }
 
