e5115fb4ab70400e4b02c12bd3fbd6160d393d50
Yue Yang <yue@Yues-MacBook-Pro.local>
2015-11-14 19:14:00 +0800

    more in copyscript

--- copyscript.sh
+++ copyscript.sh
@@ -5,4 +5,5 @@ make
 cp firmware.bin /Volumes/disk3s1
 diskutil unmount /dev/disk3s2
 sudo dd if=kernel.elf of=/dev/disk3s2 bs=2m count=1
+sudo dd if=mbr.bin of=/dev/disk3s1 bs=2m count=1
 diskutil unmountDisk /dev/disk3
